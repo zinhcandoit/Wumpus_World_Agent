3ded4d93e5ce7d3ad08dcebb0d5cbea21307d678
Vinh <quangvinhthieu@gmail.com>
2025-08-15 18:11:32 +0700

    Game does not hold action list but Agent does

--- main_process.py
+++ main_process.py
@@ -1,5 +1,4 @@
 from Development.gameState import Game
-from Development.definition import Literal
 def print_map(map):
     for row in map.grid:
         print(" | ".join(" ".join(cell) for cell in row))
@@ -14,22 +13,22 @@ def play(size, pit_density, num_wumpus):
         game.agent_take_percepts()  # Agent takes percepts from the map
         game.agent.get_KB_from_percepts()
         action = game.agent.choose_action('random')   # We will use random agent / Logic agent for choosing action
-        game.actions.append(action)
+        game.agent.actions.append(action)
         game.update_score()
         if action == "climb out":
             break
         flag = game.map.update_map(action, game.agent)
-        print('Iter', len(game.actions), 'Action:', action)
+        print('Iter', len(game.agent.actions), 'Action:', action)
         print_map(game.map)
         if flag == False:
-            game.actions.append("die")
+            game.agent.actions.append("die")
             game.update_score()
             break
-        if (len(game.actions) > 50):
+        if (len(game.agent.actions) > 30):
             print("Too many actions, stopping the game.")
             break
-    return game.point, game.actions
+    return game.point, game.agent.actions
 
-point, action = play(4, 0, 3)  # Example call to play the game with a 4x4 map, 20% pit density, and 2 wumpuses
+point, action = play(4, 0.2, 2)  # Example call to play the game with a 4x4 map, 20% pit density, and 2 wumpuses
 print(f"Final score: {point}")
 print(f"Actions taken: {action}")
