2c52a2a5d540001154e8cddf2249a70e160f4fb2
Vinh <quangvinhthieu@gmail.com>
2025-08-15 18:16:04 +0700

    add the time when pushing Literal to percept and KB

--- Development/definition.py
+++ Development/definition.py
@@ -6,6 +6,7 @@ class Literal:
     name: str
     pos: Tuple[int, int]
     negate: bool = False
+    at_step: int = 0 # Used to reset knowledge when wumpus change (Shoot / Wumpus Move)
     
     def __repr__(self):
         return f"{'¬' if self.negate else ''}{self.name}{self.pos}"
@@ -15,7 +16,8 @@ class Literal:
             return False
         return (self.name == other.name and 
                 self.pos == other.pos and 
-                self.negate == other.negate)
+                self.negate == other.negate
+                and self.at_step == self.at_step)
     
     def __hash__(self):
         return hash((self.name, self.pos, self.negate))
