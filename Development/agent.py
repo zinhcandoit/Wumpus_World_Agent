da0a6f2aa416c3d6f5881311ab085653d3394db5
Vinh <quangvinhthieu@gmail.com>
2025-08-19 21:47:12 +0700

    Renew dedupe_latest_dynamic_inplace

--- Development/agent.py
+++ Development/agent.py
@@ -1,6 +1,7 @@
 from Development.definition import Literal
 import random
 from Development.algorithm import *
+from heapq import heappush, heappop
 class Agent:
     def __init__(self, num_w=2, map_size=None):
         self.start_location = (map_size - 1, 0)
@@ -15,6 +16,7 @@ class Agent:
         self.visited = set()
         self.percepts = [] # New percepts at current location
         self.KB = set()
+        self.current_plan = [] # Plan for actions
     
     def update_direction(self, action):
         directions = ['N', 'E', 'S', "W"]
@@ -36,6 +38,7 @@ class Agent:
         self.KB.add(make_clause([Literal("wumpus", tuple(current_pos), True, at_step)]))
         self.KB.add(make_clause([Literal("pit", tuple(current_pos), True, at_step)]))
         # negative inferences
+        # No adding -stench and -breeze here to KB
         if "stench" not in percept_names:
             for dy, dx in look_around:
                 nb = (current_pos[0] + dy, current_pos[1] + dx)
@@ -47,7 +50,7 @@ class Agent:
                     c = make_clause([Literal("wumpus", nb, True, at_step)])
                     if c:
                         self.KB.add(c)
-            self.KB.add(make_clause([Literal("stench", tuple(current_pos), True, at_step)]))
+            self.KB.add(make_clause([Literal('stench', self.location, True, at_step)]))
 
         if "breeze" not in percept_names:
             for dy, dx in look_around:
@@ -60,8 +63,7 @@ class Agent:
                     c = make_clause([Literal("pit", nb, True, at_step)])
                     if c:
                         self.KB.add(c)
-            self.KB.add(make_clause([Literal("breeze", tuple(current_pos), True, at_step)]))
-
+            self.KB.add(make_clause([Literal('breeze', self.location, True, at_step)]))
         # process each percept
         for percept in self.percepts:
             ppos = tuple(percept.pos)
@@ -70,12 +72,11 @@ class Agent:
                 c = make_clause([Literal("gold", ppos, False, at_step)])
                 if c:
                     self.KB.add(c)
-
             if percept.name == 'stench':
                 or_literals = []
                 for dy, dx in look_around:
                     nb = (ppos[0] + dy, ppos[1] + dx)
-                    if 0 <= nb[0] < self.size_known and 0 <= nb[0] < self.size_known: 
+                    if 0 <= nb[0] < self.size_known and 0 <= nb[1] < self.size_known: 
                         or_literals.append(Literal("wumpus", nb, False, at_step))
                         impl = make_clause([Literal("wumpus", nb, True, at_step), Literal("stench", ppos, False, at_step)])
                         if impl:
@@ -92,12 +93,11 @@ class Agent:
                     self.KB.add(c)
                 self.KB.add(make_clause([Literal("stench", ppos, False, at_step)]))
                 self.KB.add(make_clause([Literal("wumpus", ppos, True, at_step), Literal("pit", ppos, True, at_step)]))
-
             if percept.name == 'breeze':
                 or_literals = []
                 for dy, dx in look_around:
                     nb = (ppos[0] + dy, ppos[1] + dx)
-                    if 0 <= nb[0] < self.size_known and 0 <= nb[0] < self.size_known: 
+                    if 0 <= nb[0] < self.size_known and 0 <= nb[1] < self.size_known: 
                         or_literals.append(Literal("pit", nb, False, at_step))
                         impl = make_clause([Literal("pit", nb, True, at_step), Literal("breeze", ppos, False, at_step)])
                         if impl:
@@ -113,7 +113,6 @@ class Agent:
                     self.KB.add(c)
                 self.KB.add(make_clause([Literal("breeze", ppos, False, at_step)]))
                 self.KB.add(make_clause([Literal("wumpus", ppos, True, at_step), Literal("pit", ppos, True, at_step)]))
-
             if percept.name == 'scream':
                 die_pos = percept.pos
                 c = make_clause([Literal("wumpus", die_pos, True, at_step + 1)])
@@ -134,39 +133,241 @@ class Agent:
         self.percepts = []
 
     def dedupe_latest_dynamic_inplace(self):
-        latest_time = {}
-        for clause in list(self.KB):
+        t = len(self.actions)
+        latest_any = {}
+        latest_no_wumpus = {}
+
+        for clause in self.KB:
             for lit in clause:
                 if lit.name in dynamic_literal:
-                    key = (lit.name, lit.pos, lit.negate)
-                    if key not in latest_time or lit.at_step > latest_time[key]:
-                        latest_time[key] = lit.at_step
+                    k = (lit.name, lit.pos)
+                    if lit.at_step > latest_any.get(k, -1):
+                        latest_any[k] = lit.at_step
+                if lit.name == 'wumpus' and lit.negate:
+                    if lit.pos not in latest_no_wumpus or lit.at_step > latest_no_wumpus[lit.pos]:
+                        latest_no_wumpus[lit.pos] = lit.at_step
 
+        # Drop older wumpus clause
         new_KB = set()
         for clause in self.KB:
-            keep_clause = True
-            for lit in clause:
-                if lit.name in dynamic_literal:
-                    key = (lit.name, lit.pos, lit.negate)
-                    if lit.at_step < latest_time[key]:
-                        keep_clause = False
-                        break
-            if keep_clause:
+            outdated = any(
+                (lit.name in dynamic_literal) and
+                (lit.at_step < latest_any.get((lit.name, lit.pos), lit.at_step))
+                for lit in clause
+            )
+            if not outdated:
                 new_KB.add(clause)
         self.KB = new_KB
 
+        # Keep no wumpus clauses
+        if t > 0 and t % 5 == 0:
+            self.KB = {clause for clause in self.KB
+                    if not any((lit.name == 'stench') and (lit.at_step < t) for lit in clause)}
+
+        any_stench_at_t = any(lit.name == 'stench' and lit.negate == False and lit.at_step == t
+                      for clause in self.KB for lit in clause)
+
+        if self.actions and self.actions[-1] == 'move' and (t % 5) != 0 and any_stench_at_t == False and latest_no_wumpus:
+            # No wumpus at time t - n => no wumpus at time t 
+            existing_neg_t = {lit.pos for clause in self.KB for lit in clause
+                            if (lit.name == 'wumpus') and lit.negate and lit.at_step < t}
+            for pos, step in latest_no_wumpus.items():
+                if step != t and pos not in existing_neg_t:
+                    c = make_clause([Literal('wumpus', pos, True, t)])
+                    if c:
+                        self.KB.add(c)
+
+    @staticmethod
+    def heuristic(a, b):
+        return abs(a[0]-b[0]) + abs(a[1]-b[1])
+    
+    @staticmethod
+    def turn_cost(curr_dir, target_dir):
+        directions = ['N', 'E', 'S', 'W']
+        idx_curr = directions.index(curr_dir)
+        idx_target = directions.index(target_dir)
+
+        diff = (idx_target - idx_curr) % 4  
+
+        steps = []
+        if diff == 0:
+            steps.append("move")
+            return (1, tuple(steps))
+        elif diff == 1:
+            steps.extend(["turn right", "move"])
+            return (2, tuple(steps))
+        elif diff == 2:
+            steps.extend(["turn right", "turn right", "move"])
+            return (3, tuple(steps))
+        elif diff == 3:
+            steps.extend(["turn left", "move"])
+            return (2, tuple(steps))
+        
+    def select_goal(self, safe, unsafe, candidates):
+        """
+        Chọn goal động dựa trên trạng thái hiện tại
+        """
+        # Nếu có vàng rồi -> ưu tiên thoát
+        if self.has_gold:
+            return self.start_location
+
+        # Nếu còn ô an toàn chưa khám phá -> tiếp tục explore
+        if candidates:
+            # chọn ô gần nhất để khám phá
+            return min(candidates, key=lambda c: self.heuristic(self.location, c))
+
+        # Nếu không còn gì để explore -> quay về start
+        return self.start_location
+
+    
+    def find_next_action(self):
+        """
+        Chọn hành động tiếp theo cho Agent dựa trên A* và KB.
+        Agent không cần biết trước map size cho đến khi gặp bump.
+        """
+
+        if self.current_plan:
+            return self.current_plan.pop(0)
+
+        # ========== 1. Lấy trạng thái KB ==========
+        current_step = len(self.actions)
+        focus_pairs = build_focus_pairs_for_decision(self)
+        cell_states = classify_all_local(self.KB, current_step, focus_pairs)
+
+        safe, unsafe = set(), set()
+
+        grouped = {}
+        for (name, pos), state in cell_states.items():
+            if pos not in grouped:
+                grouped[pos] = {}
+            grouped[pos][name] = state
+
+        for pos, facts in grouped.items():
+            if facts.get("pit") == "SAFE" and facts.get("wumpus") == "SAFE":
+                safe.add(pos)
+            else: unsafe.add(pos)
+
+        safe |= self.visited
+        unsafe -= self.visited
+
+        for pos, facts in grouped.items():
+            if facts.get("pit") == "UNSAFE" or facts.get("wumpus") == "UNSAFE":
+                unsafe.add(pos)
+
+        # ========== 2. Tìm neighbors ==========
+        neighbors = {
+            'E': (self.location[0], self.location[1] + 1),
+            'W': (self.location[0] , self.location[1] - 1),
+            'N': (self.location[0] - 1, self.location[1]),
+            'S': (self.location[0] + 1, self.location[1])
+            }
+
+        # Loại bỏ ô ngoài map
+        for dir, pos in list(neighbors.items()):
+            if pos[0] < 0 or pos[1] < 0 or pos[0] > self.size_known - 1 or pos[1] > self.size_known - 1:
+                neighbors.pop(dir)
+        
+        actions = get_possible_actions_now(self, cell_states)
+
+        # ========== 3. Chiến lược ==========
+        # 3.1 Nếu đang đứng trên vàng -> nhặt
+        if 'grab' in actions:
+            return 'grab'
+
+        # 3.2 Nếu đã có vàng -> tìm đường về start
+        if self.has_gold:
+            if self.location == self.start_location and 'climb out' in actions:
+                return 'climb out'
+            goal = self.start_location  # luôn về start
+            self.current_plan = self.plan_path(self.location, self.direction, goal, safe)
+            if self.current_plan:
+                return self.current_plan.pop(0)
+
+        # 3.3 Nếu chưa có vàng -> tìm safe cell chưa thăm
+        candidates = []
+        for cell in safe:
+            if cell not in self.visited:
+                candidates.append(cell)
+            else:
+            # check neighbors của chính cell đó
+                cx, cy = cell
+                neighbor_cells = [
+                    (cx+1, cy), (cx-1, cy),
+                    (cx, cy+1), (cx, cy-1)
+                ]
+                for n in neighbor_cells:
+                    if n in safe and n not in self.visited:
+                        candidates.append(cell)
+                        break
+
+        if candidates:
+            # duyệt lần lượt từ gần đến xa
+            for target in sorted(candidates, key=lambda c: self.heuristic(self.location, c)):
+                plan = self.plan_path(self.location, self.direction, target, safe)
+                if plan:
+                    self.current_plan = plan
+                    return self.current_plan.pop(0)
+            
+
+        # 3.4 Nếu hết đường đi -> thoát
+        if self.location == self.start_location:
+            return "climb out"
+        else:
+            #fallback: quay lại start
+            plan = self.plan_path(self.location, self.direction, self.start_location, safe)
+            if plan:
+                self.current_plan = plan
+                return self.current_plan.pop(0)
+            return "climb out"
+    
+    def plan_path(self, start, start_dir, goal, safe_cells):
+        """
+        Tìm đường đi bằng A* từ start -> goal qua các safe_cells.
+        Trả về list action: ["turn left", "move", ...]
+        """
+
+        directions = ['N', 'E', 'S', 'W']
+        moves = {'N': (-1,0), 'E': (0,1), 'S': (1,0), 'W': (0,-1)}
+
+        start_state = (start, start_dir)
+        frontier = []
+        heappush(frontier, (0, start_state, []))
+        visited = set()
+
+        while frontier:
+            cost, (pos, facing), path = heappop(frontier)
+            if pos == goal:
+                return path
+            if (pos, facing) in visited:
+                continue
+            visited.add((pos, facing))
+
+            for d in directions:
+                dy, dx = moves[d]
+                new_pos = (pos[0]+dy, pos[1]+dx)
+                if new_pos not in safe_cells:
+                    continue
+                turn_steps_cost, turn_steps = self.turn_cost(facing, d)
+                new_path = path + list(turn_steps)
+                new_cost = cost + turn_steps_cost
+                heappush(frontier, (new_cost + self.heuristic(new_pos, goal),
+                                (new_pos, d), new_path))
+                
+        for d in directions:
+            dy, dx = moves[d]
+            back_pos = (start[0]+dy, start[1]+dx)
+            if back_pos in self.visited and back_pos in safe_cells:
+                # tìm plan quay về back_pos
+                return self.plan_path(start, start_dir, back_pos, safe_cells)        
+
+        return []
+
     def choose_action(self, mode='random'):
         if mode == 'random':
-            '''# Code tĩnh cho w
-            result = classify_all_literals(self.KB) 
-            for (name, pos), status in sorted(result.items()):
-                pos_str = f"({', '.join(map(str, pos))})" if pos else ""
-                print(f"{name}{pos_str}: {status}")
-            get_action = get_possible_actions(self, result)'''
-            # Code động cho w
             current_step = len(self.actions)
             focus_pairs = build_focus_pairs_for_decision(self)          
             result = classify_all_local(self.KB, current_step, focus_pairs)
+
             for (name, pos), status in sorted(result.items()):
                 pos_str = f"({', '.join(map(str, pos))})" if pos else ""
                 print(f"{name}{pos_str}: {status}")
@@ -175,11 +376,14 @@ class Agent:
             print("Possible actions:", get_action)
             if 'grab' in get_action:
                 return 'grab'
-            if 'move' in get_action and random.random() <= 0.7:
+            if 'move' in get_action and random.randint(1, 2) == 2:
                 return 'move'
             if 'climb out' in get_action and self.has_gold:
                 return 'climb out'
             elif 'climb out' in get_action: get_action.remove('climb out')
             if 'shoot' in get_action and self.has_arrow:
                 return 'shoot'
-            return get_action.pop(random.randint(0, len(get_action) - 1)) 
+            return get_action.pop(random.randint(0, len(get_action) - 1))
+        elif mode == 'logic':
+            get_action = self.find_next_action()
+            return get_action
